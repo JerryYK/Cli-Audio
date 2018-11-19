674ea5f4fb774b377cbbcac493a1c5c37d1ab4a2
Runquan Ye <yer@arch02.cis.gvsu.edu>
2018-11-19 01:23:53 -0500

    update

---
+++ exceptions.py
@@ -0,0 +1,19 @@
+#define customize python exceptions
+# Author: Runquan Ye, Alec Allain
+
+#customize exception class
+class CLI_Audio_Exception(Exception):
+    """Base class for cutomize Cli Audio exceptions"""
+    pass
+
+#an exception for the unavailable file path
+class File_Exception(CLI_Audio_Exception):
+    """The file path is not available!!!"""
+    pass
+
+#an exception for the unavailable window size
+class Size_Exception(CLI_Audio_Exception):
+    """The screen is too small to fit the application!!!"""
+    pass
+
+
--- front/FrontEnd.py
+++ front/FrontEnd.py
@@ -1,7 +1,12 @@
 import curses
 import curses.textpad
-
 import sys
+import os
+
+'''
+ Enhance the cli-audio's feature
+ Author: Runquan Ye
+'''
 
 class FrontEnd:
 
@@ -9,8 +14,17 @@ class FrontEnd:
         self.player = player
       	#self.player.play(sys.argv[1])
         curses.wrapper(self.menu)
+        self.playList = []
 
     def menu(self, args):
+        #getiing the height and width from the window itself
+        h, w = self.scr.getmaxyx()
+        
+        #check the h and w are they big enough
+        if(h < 30 or w < 30):
+            #throw the customer exception
+            raise exceptions.Size_exception
+
         self.stdscr = curses.initscr()
         self.stdscr.border()
         self.stdscr.addstr(0,0, "cli-audio",curses.A_REVERSE)
@@ -32,32 +46,42 @@ class FrontEnd:
                 self.stdscr.touchwin()
                 self.stdscr.refresh()
             elif c == ord('l'):
-                self.changeSong()
-                self.updateSong()
-                self.stdscr.touchwin()
-                self.stdscr.refresh()
+                self.createSongList()
 
     def createSongList(self):
-        changeWindow = curses.newwin(5, 40, 5, 50)
-        changeWindow.border()
-        changeWindow.addstr(0,0, "What are the song path?", curses.A_REVERSE)
-	self.stdscr.addstr(5,10, "q - Save & Quit")
+        listWindow = curses.newwin(5, 40, 5, 50)
+        listWindow.border()
+        listWindow.addstr(0, 0, "Create your favorite song list:")
+
+        listWindow.addstr(1, 10, "a - Add")
+        listWindow.addstr(2, 10, "r - Remove")
+        listWindow.addstr(3, 10, "q - Quit")
+        listWindow.addstr(4, 0, "-----------------------------------------------------------")
+        listWindow.addstr(5, 0, "Your currennt playlist:")
+        if self.playList is not None:
+            for i in self.playList:                
+                 listWindow.addstr(self.playList.index(i) + 7, 10, i)
+        
         while True:
-	    c = self.stdscr.getch()
+            c = self.stdscr.getch()
+            if c == ord('a'):
+                popWindow = curses.curses.newwin(8,40, 5, 50)
+                popWindow.border()
+                popWindow.addstr(0, 0, "What is the file path you want to add into the list?")    
+            #if c == ord('r'):
+
             if c == ord('q'):
-                self.quit()
-            elif c == ord('p'):
-                self.player.pause()
-            elif c == ord('c'):
-                self.changeSong()
-                self.updateSong()
-                self.stdscr.touchwin()
-                self.stdscr.refresh()
-            elif c == ord('l'):
-                self.changeSong()
-                self.updateSong()
-                self.stdscr.touchwin()
-                self.stdscr.refresh()
+               curses.noecho()
+               del changeWindow
+               break;
+
+        for i in self.playerList:      
+            self.changeSong()
+            self.updateSong()
+            self.stdscr.touchwin()
+            self.stdscr.refresh()
+
+         
         self.stdscr.refresh()
         curses.echo()
         path = changeWindow.getstr(1,1, 30)
